03b034ea1aca77cab221ad3229bc125ad849277f
cybermajeed <abdul.zxmajeed@gmail.com>
2026-09-12 18:08:51 +0530

    finished the update

--- commit.cmd
+++ commit.cmd
@@ -1,14 +1,7 @@
 @echo off
-set "msg=%~1"
-if "%msg%"=="" set "msg=Made changes"
 
-echo Adding files...
 git add .
 
-echo Committing...
-git commit -m "%msg%"
-
-echo Pushing to remote...
-git push
-
-echo Done!
+git commit -m "%*"
+echo ------------------------
+git push -u origin main
